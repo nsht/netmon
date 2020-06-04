5cd6fb706ebcd35f606c46640793d1ac361f933d
nishit <nishitmohanan@gmail.com>
2020-06-05 01:19:45 +0530

    added graph file

---
+++ graph.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt 
+import pickle
+
+handle = open('data.pickle', 'rb')
+data = pickle.load(handle)
+fig1, ax1 = plt.subplots()
+labels = [k for k,v in data.items()]
+ax1.pie([v for k,v in data.items()],labels=labels)
+plt.show()
