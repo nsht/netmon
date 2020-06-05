649c4467dab5bdf5ad8cfd174ac61115caa59a26
nishit <nishitmohanan@gmail.com>
2020-06-06 01:03:39 +0530

    Added datastore for ip addresses

--- graph.py
+++ graph.py
@@ -3,6 +3,8 @@ import pickle
 
 handle = open('data.pickle', 'rb')
 data = pickle.load(handle)
+print(data)
+data = data['dns']
 fig1, ax1 = plt.subplots()
 labels = [k for k,v in data.items()]
 ax1.pie([v for k,v in data.items()],labels=labels)
--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 from scapy.all import *
 import pickle
 
+
 def http_header(packet):
     # incomming https://stackoverflow.com/questions/24664893/python-scapy-sniff-only-incoming-packets
     if packet[Ether].src != Ether().src:
         # print("nope")
         return
     try:
-        handle = open('data.pickle', 'rb')
+        handle = open("data.pickle", "rb")
         data = pickle.load(handle)
         handle.close()
     except:
         print("file not found")
-        data = {}
+        data = {"dns": {}, "ip": {}}
     if DNS in packet:
         print(packet[DNS].qd.qname)
         dns = packet[DNS].qd.qname.decode()
-        if dns in data:
-            data[dns] +=1
+        if dns in data['dns']:
+            data['dns'][dns] += 1
         else:
-            data[dns] = 1
-        with open('data.pickle', 'wb') as f:
+            data['dns'][dns] = 1
+        with open("data.pickle", "wb") as f:
             pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
     elif IP in packet:
-        pass
+        ip = packet.getlayer(IP).src
+        if ip in data['ip']:
+            data['ip'][ip] += 1
+        else:
+            data['ip'][ip] = 1
         # print(packet.getlayer(IP).src)
-    with open('data.pickle', 'wb') as f:
-        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+        with open("data.pickle", "wb") as f:
+            pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
     # http_packet=str(packet)
     # if http_packet.find('GET'):
     #         return GET_print(packet)
+
 
 def GET_print(packet1):
     ret = "***************************************GET PACKET****************************************************\n"
@@ -37,11 +43,12 @@ def GET_print(packet1):
     ret += "*****************************************************************************************************\n"
     return ret
 
+
 # sniff(iface='enp3s0', prn=http_header, filter="tcp or ip host 8.8.8.8",lfilter=lambda pkt: pkt[Ether].src != Ether().src)
-sniff(iface='enp3s0', prn=http_header, filter="tcp or ip host 8.8.8.8")
+sniff(iface="enp3s0", prn=http_header, filter="tcp or ip host 8.8.8.8")
 
 
-# import matplotlib.pyplot as plt 
+# import matplotlib.pyplot as plt
 # labels = [k for k,v in data.items()]
 # ax1.pie([v for k,v in data.items()],labels=labels)
 # plt.show()
